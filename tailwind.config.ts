3673cd46fa7a46e37d948a0c6c0d3467ac2e65bf
ffirmanz <54357655+ffirmanz@users.noreply.github.com>
2025-08-20 21:03:58 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
 } satisfies Config;
